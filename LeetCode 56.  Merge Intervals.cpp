c93d809da1fefb9397b42e30d8369367179d7120
Karan Gupta <113716233+karanG69@users.noreply.github.com>
2024-06-09 00:03:17 +0600

    Create LeetCode 56.  Merge Intervals.cpp

---
+++ LeetCode 56.  Merge Intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty()){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                if(intervals[i][0]<=ans.back()[1]){
+                    ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                }
+                else{
+                    ans.push_back(intervals[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
